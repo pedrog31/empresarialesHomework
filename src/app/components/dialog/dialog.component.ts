e28c11ac11df96aacb3357ff2976d041bab8b384
pedro  gallego <desarrolloingenieria15@udea.edu.co>
2018-10-27 02:49:55 -0500

    all done

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,23 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  ngOnInit() {
+    console.log(this.data);
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
